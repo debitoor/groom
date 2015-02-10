2b80d534924b4ef7d5dc4faf044a3793f6e8a1d3
Allan Ebdrup <aeb@e-conomic.com>
2015-02-10 10:17:17 +0100

    export replacer

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = function(o){
 	return JSON.parse(JSON.stringify(o, replacer));
 };
 
+module.exports.replacer = replacer;
+
 function replacer(key, value) {
 	if (value === null || value === '') {
 		return undefined; //never return null or empty strings in our JSON
